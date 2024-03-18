22a74bddf01a6fcf2834601e863e95cbc365db75
Murad Najafli <muradnecefli05@gmail.com>
2024-03-18 16:43:16 +0400

    - Using LanguageUtils for language functionality - Refactoring code

--- app/src/main/java/com/muradnajafli/newscatcher/presentation/home/components/SearchPanel.kt
+++ app/src/main/java/com/muradnajafli/newscatcher/presentation/home/components/SearchPanel.kt
@@ -1,7 +1,5 @@
 package com.muradnajafli.newscatcher.presentation.home.components
 
-import android.content.Context
-import android.widget.Toast
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
