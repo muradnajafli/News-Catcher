959c28e9292a7e5ad0d8fbf190ba85b2a6531f72
Murad Najafli <muradnecefli05@gmail.com>
2024-04-19 14:14:35 +0400

    - ComponentActivity changed to AppCompatActivity

--- app/src/main/java/com/muradnajafli/newscatcher/MainActivity.kt
+++ app/src/main/java/com/muradnajafli/newscatcher/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.muradnajafli.newscatcher
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import com.muradnajafli.newscatcher.navigation.NewsNavigator
 import com.muradnajafli.newscatcher.ui.theme.NewsCatcherTheme
 import com.muradnajafli.newscatcher.util.LanguageUtils
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
